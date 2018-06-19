aa7309f666ec5844155caf64515df0f07d01574e
ZhaoYiMing <eyeder@163.com>
2018-06-19 23:10:33 +0800

    136 solution

---
+++ 136_Single_Number.cpp
@@ -0,0 +1,35 @@
+/*
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+Input: [2,2,1]
+Output: 1
+
+Example 2:
+Input: [4,1,2,1,2]
+Output: 4
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    
+    public:
+        int singleNumber(vector<int>& nums) {
+            int result = 0;
+            for(int i = 0; i < nums.size(); i++) {
+                result ^= nums[i];
+            }
+            return result;
+        }
+        
+};
+
+int main() {
+    Solution sol;
+    return 0;
+}
