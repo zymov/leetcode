961bceaa77e09d96698e446fae97e48adc98e674
zym <eyeder@163.com>
2019-07-01 14:48:32 +0800

    4 cpp

---
+++ cpp/4_Median_of_Two_Sorted_Arrays.cpp
@@ -0,0 +1,72 @@
+/**
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+You may assume nums1 and nums2 cannot be both empty.
+
+Example 1:
+
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
+Example 2:
+
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+The median is (2 + 3)/2 = 2.5
+*/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        if(m > n) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        if(n == 0) {
+            return nums1[m / 2];
+        }
+        int imin = 0, imax = m, half_len = (m + n + 1) / 2;
+        while (imin <= imax) {
+            int i = (imin + imax) / 2;
+            int j = half_len - i;
+
+            if (i < m && nums2[j - 1] > nums1[i]) {
+                imin = i + 1;
+            } else if (i > 0 && nums1[i - 1] > nums2[j]) {
+                imax = i - 1;
+            } else {
+                int max_of_left, min_of_right;
+                if (i == 0) {
+                    max_of_left = nums2[j - 1];
+                } else if (j == 0) {
+                    max_of_left = nums1[i - 1];
+                } else {
+                    max_of_left = max(nums1[i - 1], nums2[j - 1]);
+                }
+
+                if ((m + n) % 2 == 1) {
+                    return max_of_left;
+                }
+                if (i == m) {
+                    min_of_right = nums2[j];
+                } else if (j == n) {
+                    min_of_right = nums1[i];
+                } else {
+                    min_of_right = min(nums1[i], nums2[j]);
+                }
+
+                return (max_of_left + min_of_right) / 2.0;
+            }
+        }
+        return 0.0;
+    }
+};
