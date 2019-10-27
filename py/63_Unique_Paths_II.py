49c7a22b2a1a5230d6156eab37026c6f6c804d3f
zym <eyeder@163.com>
2019-10-27 15:15:54 +0800

    63 py

---
+++ py/63_Unique_Paths_II.py
@@ -0,0 +1,19 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        if not len(obstacleGrid) or not len(obstacleGrid[0]):
+            return 0
+        path, rows, columns = [0], len(obstacleGrid), len(obstacleGrid[0])
+        for i in range(columns):
+            if obstacleGrid[0][i]:
+                for j in range(i, columns):
+                    path.append(0)
+                break
+            path.append(1)
+        for i in range(1, rows):
+            for j in range(columns):
+                if obstacleGrid[i][j]:
+                    path[j + 1] = 0
+                else:
+                    path[j + 1] = path[j] + path[j + 1]
+        return path[columns]
+        
