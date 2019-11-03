65b1a46990c1de4ce663c917cefb04f411c58e7b
zym <eyeder@163.com>
2019-11-03 20:43:01 +0800

    80 py

---
+++ py/80_Remove_Duplicates_from_Sorted_Array_II.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) < 3:
+            return len(nums)
+        insertPos = 1
+        for i in range(1, len(nums) - 1):
+            if nums[i - 1] != nums[i + 1]:
+                nums[insertPos] = nums[i]
+                insertPos += 1
+        nums[insertPos] = nums[-1]
+        return insertPos + 1
