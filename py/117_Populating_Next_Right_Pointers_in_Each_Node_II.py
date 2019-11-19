22fb6f841a66fa1c49e4b5dfd05371ea2de64759
zym <eyeder@163.com>
2019-11-19 21:06:24 +0800

    117 py

---
+++ py/117_Populating_Next_Right_Pointers_in_Each_Node_II.py
@@ -0,0 +1,26 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return
+        q = []
+        tail = root
+        q.append(root)
+        while len(q):
+            node = q.pop(0)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+            if node == tail:
+                node.next = None
+                tail = q[-1] if len(q) > 0 else None
+            else:
+                node.next = q[0]
+        return root
