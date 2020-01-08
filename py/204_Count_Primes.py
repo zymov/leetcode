786fe0d1ab0c2f84e76292322a4f81aed4b6b823
zym <eyeder@163.com>
2020-01-08 20:31:14 +0800

    204 py

---
+++ py/204_Count_Primes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n < 3:
+            return 0
+        primes = [1]*n
+        primes[0] = primes[1] = 0
+        for i in range(2, n // 2 + 1):
+            if primes[i]:
+                for j in range(i*i, n, i):
+                    primes[j] = 0
+        return sum(primes)
