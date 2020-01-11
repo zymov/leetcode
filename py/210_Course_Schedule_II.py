eed4d9672bf913bcfd83c7bad65d88d6c3c89638
zym <eyeder@163.com>
2020-01-11 13:27:32 +0800

    210 py

---
+++ py/210_Course_Schedule_II.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        course = {i: set() for i in range(numCourses)}
+        pre = collections.defaultdict(set)
+
+        for i, j in prerequisites:
+            course[i].add(j)
+            pre[j].add(i)
+
+        queue = collections.deque([i for i in course if not course[i]])
+        count, res = 0, []
+
+        while queue:
+            node = queue.popleft()
+            res.append(node)
+            count += 1
+            for i in pre[node]:
+                course[i].remove(node)
+                if not course[i]:
+                    queue.append(i)
+        return res if count == numCourses else []
