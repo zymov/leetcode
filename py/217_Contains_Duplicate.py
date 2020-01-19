846af0e96cc22af925d7d3ad5637979aeaa06ca1
zym <eyeder@163.com>
2020-01-19 19:42:16 +0800

    217 py v2

--- py/217_Contains_Duplicate.py
+++ py/217_Contains_Duplicate.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         m = {}
@@ -7,4 +8,13 @@ class Solution:
             if m.get(i):
                 return True
             m[i] = True
+        return False
+
+    def containsDuplicate2(self, nums: List[int]) -> bool:
+        if len(nums) < 2:
+            return False
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True
         return False
