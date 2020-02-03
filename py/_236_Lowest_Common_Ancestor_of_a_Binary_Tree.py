157d97151130b6d3a0bcf5f81fa2a7a8403c0cd1
zym <eyeder@163.com>
2020-02-03 11:53:37 +0800

    _236 iterative py

--- py/_236_Lowest_Common_Ancestor_of_a_Binary_Tree.py
+++ py/_236_Lowest_Common_Ancestor_of_a_Binary_Tree.py
@@ -32,11 +32,14 @@ p and q are different and both values will exist in the binary tree.
 """
 
 # Definition for a binary tree node.
+
+
 class TreeNode:
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
+
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
@@ -50,3 +53,23 @@ class Solution:
             return l
         else:
             return r
+
+    def lowestCommonAncestor_iter(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        stack = [root]
+        parent = {root: None}
+        while p not in parent or q not in parent:
+            node = stack.pop()
+            if node.left:
+                parent[node.left] = node
+                stack.append(node.left)
+            if node.right:
+                parent[node.right] = node
+                stack.append(node.right)
+
+        ancestor = set()
+        while p:
+            ancestor.add(p)
+            p = parent[p]
+        while q not in ancestor:
+            q = parent[q]
+        return q
